f2be62c7e7ea25b818425d1ab279037ae45796f8
Joel Piovesan <joelpiovesan@gmail.com>
2021-02-20 15:12:31 -0500

    more admin components

---
+++ geebee-shipping-client/src/components/admin/admin-dashboard/admin-dashboard.js
@@ -0,0 +1,17 @@
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function AdminDashboard() {
+    return (
+            <Row className="justify-content-md-center">
+                <Col md="6">
+                    <h1>AdminDashboard</h1>
+                </Col>
+            </Row>
+    );
+}
+
+export default AdminDashboard;
