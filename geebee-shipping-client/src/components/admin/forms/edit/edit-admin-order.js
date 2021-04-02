252fcc5e05f41f86e018db6c1ee29188fb78df19
Rachel Titco <rachel.titco@gmail.com>
2021-04-02 14:08:24 -0400

    edit admin order working

--- geebee-shipping-client/src/components/admin/forms/edit/edit-admin-order.js
+++ geebee-shipping-client/src/components/admin/forms/edit/edit-admin-order.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import React, { Component } from 'react';
+import OrderSchedule from '../schedule-table'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import axios from 'axios';
 import { Redirect, Link } from "react-router-dom";
-import logo from '../../../shared/profile/gb.png'
+import logo from '../../../shared/profile/gb.png';
+import { Typeahead } from 'react-bootstrap-typeahead';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+import Form from 'react-bootstrap/Form';
 
 
 class EditAdminOrderForm extends Component {
@@ -40,8 +42,76 @@ class EditAdminOrderForm extends Component {
       errorDestCity: '',
       errorDestPostalCode: '',
 
+      roleRequest: "Driver",
+
+      data: [],
+      driverData: [],
+      truckData: [],
+      // loading: true,
+      singleSelections: [],
+
     }
   }
+
+  //===================================GET DB DATA===================================//
+  getOrderData = async () => {
+    const orderRes = await axios.get(`http://localhost:8081/orders/${this.state.deliveryDate}`)
+    this.setState({ data: orderRes.data })
+  }
+
+  getDrivers = async () => {
+    const driverRes = await axios.get(`http://localhost:8081/users/${this.state.roleRequest}`)
+    let myDrivers = [];
+    let currentID = 0;
+
+    driverRes.data.forEach(element => {
+      let driver = element.email
+      myDrivers.push({ id: currentID, label: driver })
+      currentID++;
+    });
+    this.setState({ driverData: myDrivers })
+  }
+
+  async getLicensePlates(classNum) {
+    const truckRes = await axios.get(`http://localhost:8081/fleet/${classNum}`)
+    let myTrucks = [];
+    let currentID = 0;
+
+    truckRes.data.forEach(element => {
+      let truck = element.license_plate
+      myTrucks.push({ id: currentID, label: truck })
+      currentID++;
+    });
+    this.setState({ truckData: myTrucks })
+  }
+
+  componentDidMount() {
+    this.getOrderData()
+    this.getDrivers()
+  }
+
+  //===================================SET AUTOCOMPLETE SELECTIONS===================================//
+
+  setDriverSelections = (mySelection) => {
+    if (mySelection != undefined && mySelection[0] != undefined) {
+      this.setState({
+        driverSelections: [mySelection],
+        assigned_truckDriver: mySelection[0].label
+      })
+    }
+  }
+
+  setTruckSelections = (mySelection) => {
+    if (mySelection != undefined && mySelection[0] != undefined) {
+      this.setState({
+        truckSelections: [mySelection],
+        assigned_truckPlate: mySelection[0].label
+      })
+    }
+  }
+
+  //===================================VALIDATE INPUT FUNCTIONS===================================//
+
 
   validateStringInput = (regexStr, strInput) => {
     if (strInput < 1) {
@@ -54,6 +124,7 @@ class EditAdminOrderForm extends Component {
   }
 
   changeTruckClass = (event) => {
+    this.getLicensePlates(event.target.value)
     this.setState({
       assigned_truckClass: event.target.value
     })
@@ -72,6 +143,8 @@ class EditAdminOrderForm extends Component {
     })
   }
 
+  //===================================onSubmit()===================================//
+
   onSubmit = (event) => {
     // prevents form from acting in default way, stops refreshing
     event.preventDefault()
@@ -80,7 +153,8 @@ class EditAdminOrderForm extends Component {
     let plateValid = false
     let driverValid = false
 
-    //Validation
+    //===================================VALIDATION===================================//
+
     //Check Truck Class
     if (this.state.assigned_truckClass.length < 1) {
       this.setState({
@@ -119,7 +193,7 @@ class EditAdminOrderForm extends Component {
       })
     }
 
-    //POST DATA - put id for edit/delete
+    //===================================POST DATA===================================//
     if (classValid && plateValid && driverValid) {
       const orderData = {
         id: this.state.id,
@@ -165,128 +239,191 @@ class EditAdminOrderForm extends Component {
     else if (this.state.currentUser.role != "Admin") {
       return <Redirect to='/client' />
     }
-    else if (this.state.updateSuccess === true) {
+    else if (this.state.updateSuccess == true) {
       return <Redirect to='/admin/order-manager' />
     }
 
+    //Schedule Table React-Table Columns
+    const columns = [
+      {
+        Header: 'Driver',
+        accessor: 'assigned_truck_driverEmail',
+      },
+      {
+        Header: 'Assigned Truck',
+        accessor: 'assigned_truck_plate',
+      },
+      {
+        Header: 'Origin Address',
+        accessor: data => data.origin_address + ', ' + data.origin_city + ', ' + data.origin_postalCode
+      },
+      {
+        Header: 'Destination Address',
+        accessor: data => data.destination_address + ', ' + data.destination_city + ', ' + data.destination_postalCode,
+      }
+    ]
+
     return (
       <div>
-        <p className="text-center">
-          <img src={logo} alt='logo' />
-        </p>
-        <h1 className='text-center'>Schedule Delivery Order</h1>
+        <Row>
+          {/* Form Left Side */}
+          <Col md="6">
+            <h4 className='text-center'>Schedule Delivery Order</h4>
 
-        <div>
-          <Row className="justify-content-center">
-            <Col md="6">
-              <form onSubmit={this.onSubmit}>
+            <Row className="justify-content-center">
+              <Col md="6">
+                <Form onSubmit={this.onSubmit}>
 
-                <label>Delivery Date: </label>
-                <input type='date'
-                  disabled
-                  value={this.state.deliveryDate}
-                  className='form-control form-group' />
-
-                {/* Origin */}
-                <label>Origin Address: </label>
-                <input type='text'
-                  disabled
-                  value={this.state.origin_address}
-                  className='form-control form-group' />
-
-                <Row>
-                  <Col md="6">
-                    <label>Origin City: </label>
-                    <input type='text'
+                  <Form.Group>
+                    <Form.Label>Delivery Date: </Form.Label>
+                    <Form.Control type='date'
                       disabled
-                      value={this.state.origin_city}
+                      value={this.state.deliveryDate}
                       className='form-control form-group' />
-                  </Col>
-                  <Col md="6">
-                    <label>Origin Postal Code: </label>
-                    <input type='text'
+                  </Form.Group>
+
+                  <Row>
+                    <Col md="6">
+                      <Form.Group>
+                        <Form.Label>Cargo Type: </Form.Label>
+                        <Form.Control type='text'
+                          disabled
+                          value={this.state.cargo_type}
+                          className='form-control form-group' />
+                      </Form.Group>
+                    </Col>
+
+                    <Col md="6">
+                      <Form.Group>
+                        <Form.Label>Cargo Weight (kg):</Form.Label>
+                        <Form.Control type='text'
+                          disabled
+                          value={this.state.cargo_weight}
+                          className='form-control form-group' />
+                      </Form.Group>
+                    </Col>
+                  </Row>
+
+                  {/* Origin */}
+                  <h6>Origin:</h6>
+                  <Form.Group>
+                    <Form.Label>Address: </Form.Label>
+                    <Form.Control type='text'
                       disabled
-                      value={this.state.origin_postalCode}
+                      value={this.state.origin_address}
                       className='form-control form-group' />
-                  </Col>
-                </Row>
+                  </Form.Group>
 
-                {/* Destination */}
-                <label>Destination Address: </label>
-                <input type='text'
-                  disabled
-                  value={this.state.dest_address}
-                  className='form-control form-group' />
+                  <Row>
+                    <Col md="6">
+                      <Form.Group>
+                        <Form.Label>City: </Form.Label>
+                        <Form.Control type='text'
+                          disabled
+                          value={this.state.origin_city}
+                          className='form-control form-group' />
+                      </Form.Group>
+                    </Col>
 
-                <Row>
-                  <Col md="6">
-                    <label>Destination City: </label>
-                    <input type='text'
+                    <Col md="6">
+                      <Form.Group>
+                        <Form.Label>Postal Code: </Form.Label>
+                        <Form.Control type='text'
+                          disabled
+                          value={this.state.origin_postalCode}
+                          className='form-control form-group' />
+                      </Form.Group>
+                    </Col>
+                  </Row>
+
+                  {/* Destination */}
+                  <h6>Destination:</h6>
+                  <Form.Group>
+                    <Form.Label>Address: </Form.Label>
+                    <Form.Control type='text'
                       disabled
-                      value={this.state.dest_city}
+                      value={this.state.dest_address}
                       className='form-control form-group' />
-                  </Col>
-                  <Col md="6">
-                    <label>Destination Postal Code: </label>
-                    <input type='text'
-                      disabled
-                      value={this.state.dest_postalCode}
-                      className='form-control form-group' />
-                  </Col>
-                </Row>
+                  </Form.Group >
 
-                <Row>
-                  <Col md="6">
-                    <label>Cargo Type: </label>
-                    <input type='text'
-                      disabled
-                      value={this.state.cargo_type}
-                      className='form-control form-group' />
-                  </Col>
-                  <Col md="3">
-                    <label>Cargo Weight (in kg): </label>
-                    <input type='text'
-                      disabled
-                      value={this.state.cargo_weight}
-                      className='form-control form-group' />
-                  </Col>
+                  <Row>
+                    <Col md="6">
+                      <Form.Group>
+                        <Form.Label>City: </Form.Label>
+                        <Form.Control type='text'
+                          disabled
+                          value={this.state.dest_city}
+                          className='form-control form-group' />
+                      </Form.Group>
+                    </Col>
 
-                </Row>
+                    <Col md="6">
+                      <Form.Group>
+                        <Form.Label>Postal Code: </Form.Label>
+                        <Form.Control type='text'
+                          disabled
+                          value={this.state.dest_postalCode}
+                          className='form-control form-group' />
+                      </Form.Group>
+                    </Col >
+                  </Row >
 
-                <label>Truck Class: <span className="text-center alert-danger">{this.state.errorTruckClass}</span></label>
-                <select className='form-control form-group' value={this.state.assigned_truckClass} name="class" onChange={this.changeTruckClass}>
-                  <option disabled hidden value="">Select a truck class.</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                </select>
+                  <Form.Group>
+                    <Form.Label>Truck Class: <span className="text-center alert-danger">{this.state.errorTruckClass}</span></Form.Label>
+                    <select className='form-control form-group'
+                      value={this.state.assigned_truckClass}
+                      name="class"
+                      onChange={this.changeTruckClass}
+                    >
+                      <option disabled selected hidden value="">Select a truck class.</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                    </select>
+                  </Form.Group >
 
-                <label>License Plate: <span className="text-center alert-danger">{this.state.errorTruckPlate}</span></label>
-                <input type='text'
-                  placeholder='PLATE###'
-                  onChange={this.changeTruckPlate}
-                  value={this.state.assigned_truckPlate}
-                  className='form-control form-group' />
+                  <Form.Group>
+                    <Form.Label>License Plate: <span className="text-center alert-danger">{this.state.errorTruckPlate}</span></Form.Label>
+                    <Typeahead
+                      id="basic-typeahead-single"
+                      onChange={this.setTruckSelections}
+                      options={this.state.truckData}
+                      placeholder="PLATE###"
+                      selected={this.truckSelections}
+                    />
+                  </Form.Group >
 
-                <label>Driver Email: <span className="text-center alert-danger">{this.state.errorTruckDriver}</span></label>
-                <input type='email'
-                  placeholder='john.doe@email.com'
-                  onChange={this.changeTruckDriver}
-                  value={this.state.assigned_truckDriver}
-                  className='form-control form-group' />
+                  <Form.Group>
+                    <Form.Label>Driver Email: <span className="text-center alert-danger">{this.state.errorTruckDriver}</span></Form.Label>
+                    <Typeahead
+                      id="basic-typeahead-single"
+                      onChange={this.setDriverSelections}
+                      options={this.state.driverData}
+                      placeholder="Driver"
+                      selected={this.driverSelections}
+                    />
+                  </Form.Group>
 
-                <span className="text-center alert-danger">{this.state.errorMessage}</span>
+                  <span className="text-center alert-danger">{this.state.errorMessage}</span>
 
-                <input type='submit' className='btn btn-primary btn-block'
-                  value='Submit' />
-                <Link className="mt-3 btn btn-warning btn-block" to='/admin/order-manager'>Back</Link>
-              </form>
-            </Col>
-          </Row>
-        </div>
-      </div>
+                  <input type='submit' className='btn btn-primary btn-block'
+                    value='Submit' />
+                  <Link className="mt-3 btn btn-warning btn-block" to='/admin/order-manager'>Back</Link>
+                </Form >
+              </Col >
+            </Row >
+          </Col >
+
+          {/* Schedule Table Right Side */}
+          < Col md="6" >
+            <label><h4>Current Deliveries on: {this.state.deliveryDate}</h4></label>
+            <OrderSchedule columns={columns} data={this.state.data} />
+          </Col >
+        </Row >
+
+      </div >
 
     );
   }
